5dc7df231b323d66cd1f967d440a7cf73210e980
Gianni Gambetti <gianni@gambetti.ca>
2021-09-27 19:44:38 +0200

    Adds RegexType iteration
    Allows iterating over all the possible values of a RegexType.

--- exploder.py
+++ exploder.py
@@ -1,9 +1,34 @@
 from dataclasses import dataclass
+from functools import reduce
+from itertools import accumulate
+
+from _.command_line.flags import Flag
+
+FLAG_max_repeat = Flag.int('max-repeat', default=5,
+                           description="For infinitely repeating regex values (*, +) caps the number of repeats.")
+
 
 class RegexType:
   def regex(self) -> str:
     "To construct a regex"
-    # Needs to be implmented
+    # Needs to be implmented by subclass
+    raise Exception(NotImplemented)
+
+  def __iter__(self):
+    return RegexIter(self)
+
+  def _range_check(self, index):
+    if index >= self.choices():
+      raise IndexError("Index out of bounds for {}: {}".format(type(self), index))
+
+  def choices(self):
+    """Number of possible outputs for this regex type."""
+    # Needs to be implmented by subclass
+    raise Exception(NotImplemented)
+
+  def __getitem__(self, index):
+    """Used for iteration and random values."""
+    # Needs to be implmented by subclass
     raise Exception(NotImplemented)
 
   def __add__(self, other):
@@ -26,6 +51,20 @@ class RegexType:
     return Sum(self, other)
 
 
+class RegexIter:
+  def __init__(self, regex):
+    self.__regex = regex
+    self.__index = 0
+
+  def __next__(self):
+    i = self.__index
+    self.__index += 1
+    try:
+      return self.__regex[i]
+    except IndexError:
+      raise StopIteration
+
+
 class Value(RegexType, str):
   def __mul__(self, other):
     if isinstance(other, int):
@@ -37,19 +76,39 @@ class Value(RegexType, str):
       return Value(str(self) + str(other))
     return super().__add__(other)
 
+  def choices(self):
+    return 1
+
+  def __getitem__(self, index):
+    super()._range_check(index)
+    return str(self)
+
   def regex(self):
     return "(" + str(self) + ")"
 
 
 class CharacterRange(RegexType):
   def __init__(self, start, end):
-    self.start = start
-    self.end = end
+    if ord(start) > ord(end):
+      raise ValueError(
+          "start of range must be smaller than the end of range: {} ({}) > {} ({})".format(
+              start, ord(start), end, ord(end)))
+    self.start = ord(start)
+    self.end = ord(end)
+
+  def choices(self):
+    # inclusive range
+    return self.end - self.start + 1
+
+  def __getitem__(self, index):
+    super()._range_check(index)
+    return chr(self.start + index)
 
   def regex(self):
-    return "[" + self.start + "-" + self.end + "]"
+    return "[" + chr(self.start) + "-" + chr(self.end) + "]"
 
 
+# TODO: Remove negation support.
 class CharacterSet(RegexType):
   # Special meta characters to move to the end
   __to_remove = list("-^")
@@ -63,6 +122,15 @@ class CharacterSet(RegexType):
     self.values = ''.join(values) + ''.join(bad)
     self.negated = negated
 
+  def choices(self):
+    if self.negated:
+      raise Exception(NotImplemented)
+    return len(self.values)
+
+  def __getitem__(self, index):
+    super()._range_check(index)
+    return self.values[index]
+
   def regex(self):
     n = ''
     if self.negated:
@@ -73,6 +141,12 @@ class CharacterSet(RegexType):
 class Product(RegexType):
   def __init__(self, *values):
     self.values = values
+    self.__c = None
+
+  def __choices(self):
+    if self.__c is None:
+      self.__c = [v.choices() for v in self.values]
+    return self.__c
 
   def regex(self):
     parts = [p.regex() for p in self.values]
@@ -85,10 +159,29 @@ class Product(RegexType):
       return Product(*self.values, *other.values)
     return Product(*self.values, other)
 
+  def __getitem__(self, index):
+    super()._range_check(index)
+    outputs = []
+    for i, weight in enumerate(self.__choices()):
+      option = index % weight
+      index = index // weight
+      outputs.append(self.values[i][option])
+
+    return ''.join(outputs)
+
+  def choices(self):
+    return reduce(lambda x, y: x * y, self.__choices(), 1)
+
 
 class Sum(RegexType):
   def __init__(self, *values):
     self.values = values
+    self.__c = None
+
+  def __choices(self):
+    if self.__c is None:
+      self.__c = [v.choices() for v in self.values]
+    return self.__c
 
   def regex(self):
     parts = [p.regex() for p in self.values]
@@ -100,6 +193,19 @@ class Sum(RegexType):
     if isinstance(other, Sum):
       return Sum(*self.values, *other.values)
     return Sum(*self.values, other)
+
+  def __getitem__(self, index):
+    super()._range_check(index)
+    weights = self.__choices()
+    option = 0
+    while index >= weights[option]:
+      index -= weights[option]
+      option += 1
+
+    return self.values[option][index]
+
+  def choices(self):
+    return sum(self.__choices())
 
 
 @dataclass
@@ -149,7 +255,40 @@ class Repeated(RegexType):
     self.value = value
     self.repeat = repeat
 
+  def __count(self):
+    count = self.repeat.max
+    if count is None:
+      count = max(FLAG_max_repeat.value, self.repeat.min)
+    # Max is inclusive.
+    return count - self.repeat.min + 1
+
   def regex(self):
     return self.value.regex() + self.repeat.regex()
 
+  def __getitem__(self, index):
+    super()._range_check(index)
+    choices = self.value.choices()
 
+    # Handle 1 differently
+    if choices == 1:
+      val = self.value[0]
+      return val * self.repeat.min + val * index
+
+    outputs = []
+    # Mandatory repeats, just like Product.
+    for _i in range(self.repeat.min):
+      option = index % choices
+      index = index // choices
+      outputs.append(self.value[option])
+
+    # Optional repeats.
+    while index > 0:
+      option = index % choices
+      index = index // choices
+
+      outputs.append(self.value[option])
+
+    return ''.join(outputs)
+
+  def choices(self):
+    return self.value.choices() * self.__count()
--- test_exploder.py
+++ test_exploder.py
@@ -1,10 +1,33 @@
 import unittest
 
-from _.language.regex_gen.exploder import CharacterRange, CharacterSet, Repeat
+from _.language.regex_gen.exploder import CharacterRange, CharacterSet, Repeat, Value, Sum, Product
+from _.language.regex_gen.exploder import FLAG_max_repeat
+
+class ValueTests(unittest.TestCase):
+  def test_choices(self):
+    v = Value("abc")
+    self.assertEqual(v.choices(), 1)
+
+  def test_iter(self):
+    v = Value("abc")
+    self.assertEqual(v.choices(), 1)
+    self.assertEqual(v[0], "abc")
+    self.assertEqual(list(v), ["abc"])
+
 
 class CharacterTests(unittest.TestCase):
+  def test_outoforder_range(self):
+    with self.assertRaises(ValueError):
+      CharacterRange("z", "a")
+
+  def test_iter_range(self):
+    self.assertEqual(set(CharacterRange("a", "d")), set(["a", "b", "c", "d"]))
+
   def test_range_regex(self):
     self.assertEqual(CharacterRange("a", "z").regex(), "[a-z]")
+
+  def test_iter_set(self):
+    self.assertEqual(set(CharacterSet("abcdefg")), set("abcdefg"))
 
   def test_set(self):
     def regex(cs):
@@ -37,4 +60,40 @@ class RepeatTests(unittest.TestCase):
     self.assertEqual(Repeat.atleast(3).regex(), "{3,}")
     self.assertEqual(Repeat.between(3, 7).regex(), "{3,7}")
     self.assertEqual(Repeat.between(3, 3).regex(), "{3}")
+
+  def test_choices(self):
+    self.assertEqual(FLAG_max_repeat.value, 5, "Test prereq failed, flag (--max-repeat) value changed.")
+    value = 100
+
+    r = Value("a") * Repeat.atleast(value)
+    self.assertEqual(r.choices(), 1)
+
+    r = Value("a") * Repeat.any()
+    self.assertEqual(r.choices(), 6) # 5 + 1
+
+  def test_iter(self):
+    r = Value("a") * Repeat(3, 5)
+    self.assertEqual(r.choices(), 3)
+    self.assertEqual(list(r), ["aaa", "aaaa", "aaaaa"])
+
+
+class CombinedTests(unittest.TestCase):
+  def test_iter(self):
+    v = Value("a")
+    p = Product(v, v, v, v)
+    self.assertEqual(p.choices(), 1)
+    self.assertEqual(set(p), set(["aaaa"]))
+
+    s = Sum(v, v, v)
+    self.assertEqual(s.choices(), 3)
+    self.assertEqual(set(s), set(["a", "a", "a"]))
+
+    r = (Value("a") | Value("b")) + Value("c")
+    self.assertEqual(r.choices(), 2)
+    self.assertEqual(set(r), set(["ac", "bc"]))
+
+    r = (Value("a") + Value("b")) | Value("c")
+    self.assertEqual(r.choices(), 2)
+    self.assertEqual(set(r), set(["ab", "c"]))
+
 
